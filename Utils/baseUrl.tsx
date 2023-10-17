5583cfa46d38caba68fbcf91d6590a5f71df0b88
charuka97 <charukatharindhu@gmail.com>
2023-10-17 19:47:47 +0530

    change base url

--- Utils/baseUrl.tsx
+++ Utils/baseUrl.tsx
@@ -1,7 +1,7 @@
-//const baseUrl = 'http://localhost:9000/api'
+const baseUrl = 'http://localhost:9000/api'
 // const baseUrl = 'https://api.bloonsoo.com/api'
 
-const baseUrl = 'http://13.49.80.171:9000/api'
+//const baseUrl = 'http://13.49.80.171:9000/api'
 
 
 export default baseUrl;
